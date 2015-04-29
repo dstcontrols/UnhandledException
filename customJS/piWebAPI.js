722eaf3dd086e4fdeb2e83823fd1ff9cd02b5d7d
Sarven Buyuker <sbuyuker@dstcontrols.com>
2015-04-29 14:29:46 -0700

    Test Get...

--- customJS/piWebAPI.js
+++ customJS/piWebAPI.js
@@ -5,5 +5,21 @@
 var result = $.ajax("https://osiproghackuc2015.osisoft.com/piwebapi/streams/A0EjSJsJ258_0mQZSscfNuctQ7RK9lpfu5BGAywANOjGGlQ5BDuQVJa7VoiDkXYVGKomATUVSQ1VSWTAwMVxEQVRBQkFTRUhBQ0tHUk9VUDExXEFOQUtJTiBTT0xPXFJVTktFRVBFUnxFTEVWQVRJT04/summary", 
 	{ 
 		type : 'GET', 
-		headers: { "Authorization" : "basic " + btoa("osiproghack\\hackuser051:bO2rA53P2")
+		headers: { "Authorization" : "Basic " + btoa("osiproghack\\hackuser051:bO2rA53P2")
 	}});
+
+
+// var result = $.ajax('https://osiproghackuc2015.osisoft.com/piwebapi/streams/A0EjSJsJ258_0mQZSscfNuctQ7RK9lpfu5BGAywANOjGGlQ5BDuQVJa7VoiDkXYVGKomATUVSQ1VSWTAwMVxEQVRBQkFTRUhBQ0tHUk9VUDExXEFOQUtJTiBTT0xPXFJVTktFRVBFUnxFTEVWQVRJT04/summary', {
+//     type: 'GET',
+//     username: 'osiproghack\\hackuser051',
+//     password: 'bO2rA53P2',
+//     dataType: "jsonp",
+//     contentType: "application/json; charset=utf-8",
+//     xhrFields: {
+//         withCredentials: false
+//     }
+// }).then(function (data) {
+//     alert('success');
+// }, function (xhr) {
+//     alert('failure');
+// });
