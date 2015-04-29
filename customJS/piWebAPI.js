25595e581575145c6ab989f72b81820ae56716eb
adkenyon <akenyon@DST-L5075.DSTControls.local>
2015-04-29 13:29:59 -0700

    fixed

---
+++ customJS/piWebAPI.js
@@ -0,0 +1,3 @@
+
+var result = $.ajax("https://osiproghackuc2015.osisoft.com/piwebapi/streams/A0EjSJsJ258_0mQZSscfNuctQ7RK9lpfu5BGAywANOjGGlQ5BDuQVJa7VoiDkXYVGKomATUVSQ1VSWTAwMVxEQVRBQkFTRUhBQ0tHUk9VUDExXEFOQUtJTiBTT0xPXFJVTktFRVBFUnxFTEVWQVRJT04/summary", 
+	{ method : 'GET', username : "osiproghack/hackuser051", password : "bO2rA53P2" });
