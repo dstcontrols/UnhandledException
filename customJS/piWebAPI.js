0e57cc7378f1aee13808948be3cc4aff484a0ff2
adkenyon <akenyon@DST-L5075.DSTControls.local>
2015-04-29 13:53:15 -0700

    ajax ready

--- customJS/piWebAPI.js
+++ customJS/piWebAPI.js
@@ -1,8 +1,9 @@
 
+
+
+
 var result = $.ajax("https://osiproghackuc2015.osisoft.com/piwebapi/streams/A0EjSJsJ258_0mQZSscfNuctQ7RK9lpfu5BGAywANOjGGlQ5BDuQVJa7VoiDkXYVGKomATUVSQ1VSWTAwMVxEQVRBQkFTRUhBQ0tHUk9VUDExXEFOQUtJTiBTT0xPXFJVTktFRVBFUnxFTEVWQVRJT04/summary", 
 	{ 
 		type : 'GET', 
-		username : "osiproghack\\hackuser051", 
-		password : "bO2rA53P2", 
 		headers: { "Authorization" : "basic " + btoa("osiproghack\\hackuser051:bO2rA53P2")
 	}});
